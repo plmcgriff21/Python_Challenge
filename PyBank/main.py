2fec75f6eefaaf8ee79782bb5ef006c63f74ec64
PLMcGriff <portiamcgriff21@gmail.com>
2020-07-03 14:33:11 -0400

    Python assignment updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,24 +9,22 @@ profit=0
 averagechange=0
 net_change_list = []
 prev_value=0
+maxmonth=[]
+
 #--------------------------------------------------------------------------------------------
 #Open the file path, initialize the reader and define how data from the file should be parsed
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
 #-------------------------------------------------------------------------------------------
 #Make the file content readable in the program and for people
-    #print results for computer consumption
-    print(csvreader)
-    #define header row to isolate it from remaining data set
+#define header row to isolate it from remaining data set
     csv_header = next(csvreader)
     firstrow=next(csvreader)
     totalmonths=totalmonths+1
     profit= profit + float(firstrow[1])
     prev_value= int(firstrow[1])
-    
-    #print header for human viewing
-    print(csv_header)
-    #create loop to find details within the data extract
+  
+#create loop to find details within the data extract
     for row in csvreader:
         #find total number of months being evaluated
         net_change= int(row[1]) -prev_value 
@@ -34,17 +32,24 @@ with open(csvpath) as csvfile:
         net_change_list.append(net_change)
         #net_change_list = net_change_list + [net_change]
         totalmonths=totalmonths+1
-        print(f'"Total Months:" {totalmonths}')
         #find total profit for the entire period
         profit= profit + float(row[1]) 
-        print(f'"Total profits:"{profit}')
-avgdiff= sum(net_change_list)/len(net_change_list)
-print(f'"Average Change:"{avgdiff}')
-MaxChange=max(net_change_list)
-MinChange=min(net_change_list)
-print(f'"Greatest increase in profits:"{MaxChange}')
-print(f'"Greatest descrease in profits:"{MinChange}')
+        MaxChange=max(net_change_list)
+        MinChange=min(net_change_list)
+        avgdiff= sum(net_change_list)/len(net_change_list)
+     
+            
+      
+        
 
+with open("financialdata.txt","a") as f:
+    print("--------------Financial Results--------------------------------------------------",file=f)
+    print(f'Total Months {totalmonths}',file=f)
+    print(f'Total profits:{profit}',file=f)
+    print(f'Average Change:{avgdiff}',file=f)
+    print(f'Greatest increase in profits: {MaxChange}',file=f)
+    print(f'Greatest descrease in profits:  {MinChange}',file=f)
+    print("---------------------------------------------------------------------------------",file=f)
 
 
 #-------------------------------------------------------------------------------------------
